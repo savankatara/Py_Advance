21503c81fdae82a525563a8fafc6229dfbd6d2be
Katara <savankatara@gmail.com>
2018-08-21 17:04:27 +0530

    git for loop

--- Day1/Foor_Loop/For11.py
+++ Day1/Foor_Loop/For11.py
@@ -2,12 +2,8 @@ a=int(raw_input("Enter your first value: "))
 b=int(raw_input("Enter your second value: "))
 print "First value %d and second value %d "%(a,b) 
 for c in range(a,b):
-    for e in range(c,5):
-        print e
-    
     if(c==2):
-        pass
-        print "pass working"
+        continue
     print c
 else:
     print "for loop completed"
--- Day1/Foor_Loop/For12.py
+++ Day1/Foor_Loop/For12.py
@@ -2,9 +2,6 @@ a=int(raw_input("Enter your first value: "))
 b=int(raw_input("Enter your second value: "))
 print "First value %d and second value %d "%(a,b) 
 for c in range(a,b):
-    for e in range(c,5):
-        print e
-    
     if(c==2):
         pass
         print "pass working"
