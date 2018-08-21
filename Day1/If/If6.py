726d53b450f0ad093baac6a3f21ad35e19097199
Katara <savankatara@gmail.com>
2018-08-21 15:37:29 +0530

    file Day if statement

---
+++ Day1/If/If6.py
@@ -0,0 +1,5 @@
+a=10
+if(a==20):
+    print "True condition"
+else:
+    print "False condition"
---
+++ Day1/If/If7.py
@@ -0,0 +1,13 @@
+a=10
+if(a==20):
+    print "True condition"
+elif(a==12):
+    print "a==12"
+elif(a==11):
+    print "a==11"
+elif(a==10):
+    print "a==10"
+elif(a==9):
+    print "a==9"
+else:
+    print "False condition"
