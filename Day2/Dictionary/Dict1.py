1d9f7a11abe6c2b7eb7a8240b593f704a3d4408b
Katara <savankatara@gmail.com>
2018-08-24 01:52:52 +0530

    dict

--- Day2/Dictionary/Dict1.py
+++ Day2/Dictionary/Dict1.py
@@ -1,8 +1,8 @@
-a={'name ':"savan",'mobile':80454646,'add':"indore mp (india)",'name ':"python",'age':1}
-b={}
-#a['age']=25
-
-print a.has_key('age')
-print a.setdefault('sex',None)
+a=('name ','mobile', 'add')
+dict=dict.fromkeys(a)
+print dict
 #a.clear()
 #print a
+
+dict=dict.fromkeys(a,50)
+print dict
